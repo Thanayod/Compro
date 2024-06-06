1e5b2ce94f954f2fd229cfe6cd7a36a60d9c8928
Thanayod <uzalam2547@gmail.com>
2024-06-06 13:34:21 +0700

    Add files via upload

---
+++ compro : Recursion/SeparateCharactersAndNumbers.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class SeparateCharactersAndNumbers {
+    static String split(String msg) {
+        String da = "0123456789";
+        if (msg.length() <= 1) {
+            return msg;
+        }
+        if (da.indexOf(msg.charAt(0)) == -1) {
+            return msg.charAt(0) + split(msg.substring(1));
+        }
+        return split(msg.substring(1)) + msg.charAt(0);
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String msg = sc.next();
+        String res = split(msg);
+        System.out.println(res);
+    }
+}
